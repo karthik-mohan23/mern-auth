332df35b301a520121d68f4a9b8cd7b5dd23438a
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-22 12:03:35 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;

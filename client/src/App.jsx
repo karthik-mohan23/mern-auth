e8cfd9b6dcb447d02858b585b0efc1d98465077e
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-22 12:33:46 +0530

    create Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-950">
+      <nav className="w-[90%] max-w-5xl mx-auto flex justify-between text-white py-3">
+        <Link to="/" className="text-xl font-bold">
+          MERN
+        </Link>
+        <ul className="flex items-center gap-16">
+          <Link to="/sign-up">Sign up</Link>
+          <Link to="/sign-in">Sign in</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
